6994bf768373b05a3878cf82822cedaf4032f76b
Senior_DEV <86672166+CJpaekil@users.noreply.github.com>
2022-05-30 12:23:24 +0800

    app updated

---
+++ client/src/pages/home.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+const Home = () => {
+    return (
+        <div className="container">
+            sfdsf
+        </div>
+    )
+}
+
+export default Home;
