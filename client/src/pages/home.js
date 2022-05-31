c7f926e20b9a4424e64cd023fa5900605073ff8c
Senior_DEV <86672166+CJpaekil@users.noreply.github.com>
2022-05-31 09:00:42 +0800

    all updated

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,7 +1,12 @@
 import { useEffect, useState } from "react";
 import FrameModal from "../component/FrameModal";
 import SelfieModal from "../component/SelfieModal";
-import mergeImages from 'merge-images';
+import SelfieResizeDrag from "../component/SelfieResize";
+import canvasToImage from 'canvas-to-image';
+import { exportComponentAsJPEG, exportComponentAsPDF, exportComponentAsPNG } from 'react-component-export-image';
+import React, { useRef } from 'react';
+
+
 
 const Home = () => {
   const [selfieimage, setSelfieImage] = useState([]);
@@ -10,6 +15,24 @@ const Home = () => {
   const [selectedFrame, setSelectedFrame] = useState(null);
   const [frameModalShow, setFrameModalShow] = useState(false);
   const [selfieModalShow, setSelfieModalShow] = useState(false);
+  const componentRef = useRef();
+
+  const ComponentToPrint = React.forwardRef((props, ref) => (
+    <>
+      {
+        backgroundimage.preview ? (
+          <img src={backgroundimage.preview} alt="dummy" width="100%" height="100%" />
+        ) : (<></>)
+      }
+      {
+        selectedFrame ? (<> <img className="selected-frame" src={selectedFrame} alt="dummy" width="100%" height="100%" /></>) : (<></>)
+      }
+      {
+        selectedSelfie ? (<SelfieResizeDrag selfiesrc={selectedSelfie} />) : (<></>)
+      }
+    </>
+  ));
+
 
   const changeSelfieHandler = (e) => {
     const temp = [];
@@ -44,9 +67,21 @@ const Home = () => {
   }
 
   const mergeTotalImages = () => {
-    alert("tyuio");
-    mergeImages([{ backgroundimage }, { selectedFrame }, { selectedSelfie }])
-      .then(b64 => document.querySelector('merge-image').src = b64);
+    const canvasEl = document.getElementById('myCanvas');
+
+    canvasToImage(canvasEl, {
+      name: 'myJPG',
+      type: 'jpg',
+      quality: 0.5
+    });
+
+    canvasToImage('myCanvas', {
+      name: 'myPNG',
+      type: 'png',
+      quality: 1
+    });
+
+    canvasToImage('myCanvas');
   }
 
   useEffect(() => {
@@ -68,11 +103,12 @@ const Home = () => {
         <div className="col-sm-4">
           <div className="image-load selfie-img">
             {
-              selfieimage.length >= 1 ? (<>
-                {
-                  selectedFrame ? (<><img src={selectedSelfie} alt="dummy" width="93%" height="97%" /><img className="selected-frame" src={selectedFrame} alt="dummy" width="100%" height="100%" /></>) : (<img src={selectedSelfie} alt="dummy" width="100%" height="100%" />)
-                }
-              </>
+              selfieimage.length >= 1 ? (
+                <>
+                  {
+                    selectedFrame ? (<><img src={selectedSelfie} alt="dummy" width="93%" height="97%" /><img className="selected-frame" src={selectedFrame} alt="dummy" width="100%" height="100%" /></>) : (<img src={selectedSelfie} alt="dummy" width="100%" height="100%" />)
+                  }
+                </>
               ) : (<></>)
             }
           </div>
@@ -105,12 +141,13 @@ const Home = () => {
           <div className="d-flex"><input type="file" name="Choose Files" accept="image/png, image/gif, image/jpeg" onChange={changeBackgroundHandler} /></div>
         </div>
         <div className="col-sm-4 combined-img">
-          <div className="image-load merge-image"></div>
-          <div className="d-flex"><button onClick={() => mergeTotalImages()}>Merge</button></div>
+          <div className="image-load" id="mergeImage">
+            <ComponentToPrint ref={componentRef} />
+          </div>
+          <div className="d-flex"><button onClick={() => mergeTotalImages()}>Export</button></div>
         </div>
       </div>
     </div>
   )
 }
-
 export default Home;
